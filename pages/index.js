1d564077c689784f4e03baf641690ea6589cba2f
rnd2131 <radmandarkhosh18@gmail.com>
2025-06-07 21:40:12 +0330

    neio

--- pages/index.js
+++ pages/index.js
@@ -6,33 +6,6 @@ import LandingLayout from "../components/LandingLayout";
 export default function Home() {
   return (
     <>
-      <NextSeo
-        title="Local PDF - Your Free PDF Editor in the Browser"
-        description="Edit PDF files in your browser with Local PDF. Merge, optimize, watermark, and more. No installation or registration needed, and your files never leave your system."
-        canonical="https://localpdf.tech/"
-        openGraph={{
-          url: "https://localpdf.tech/",
-          title: "Local PDF - Your Free PDF Editor in the Browser",
-          description:
-            "Edit PDF files in your browser with Local PDF. Merge, optimize, watermark, and more. No installation or registration needed, and your files never leave your system.",
-          type: "website",
-          images: [
-            {
-              url: "https://raw.githubusercontent.com/julianfbeck/localpdfmerger/main/public/og-image-01.png",
-              width: 1200,
-              height: 630,
-              alt: "Local PDF - Your Free PDF Editor in the Browser",
-              type: "image/jpeg",
-            },
-          ],
-          siteName: "Local PDF",
-        }}
-        twitter={{
-          handle: "@julianfbeck",
-          site: "@julianfbeck",
-          cardType: "summary_large_image",
-        }}
-      />
 
       <LandingLayout>
         <Hero
